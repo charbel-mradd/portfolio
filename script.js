ec464b24903661982a8c3371c67250c5eed29cce
charbel mrad <codebraver26@codebrave.org>
2024-12-21 01:28:24 -0800

    new

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var typed = new typed(".text",{
-    Strings:["Web Developer" , "Student" , "Worker"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Student", "Worker"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
